400631bc2985dbdb62edbe688165738a45fdaa86
Antoine Goujon <antoine.goujon@truite.polytechnique.fr>
2020-02-29 20:37:54 +0100

    c

---
+++ src/mini_c/ToRTL.java
@@ -0,0 +1,86 @@
+public class ToRTL implements Visitor {
+	public void visit(Unop n) {
+	}
+
+	public void visit(Binop n) {
+	}
+
+	public void visit(String n) {
+        
+	}
+
+	public void visit(Tint n) {
+	}
+
+	public void visit(Tstructp n) {
+	}
+
+	public void visit(Tvoidstar n) {
+	}
+
+	public void visit(Ttypenull n) {
+	}
+
+	public void visit(Structure n) {
+	}
+
+	public void visit(Field n) {
+	}
+
+	public void visit(Decl_var n) {
+	}
+
+	public void visit(Expr n) {
+	}
+
+	public void visit(Econst n) {
+	}
+
+	public void visit(Eaccess_local n) {
+	}
+
+	public void visit(Eaccess_field n) {
+	}
+
+	public void visit(Eassign_local n) {
+	}
+
+	public void visit(Eassign_field n) {
+	}
+
+	public void visit(Eunop n) {
+	}
+
+	public void visit(Ebinop n) {
+	}
+
+	public void visit(Ecall n) {
+	}
+
+	public void visit(Esizeof n) {
+	}
+
+	public void visit(Sskip n) {
+	}
+
+	public void visit(Sexpr n) {
+	}
+
+	public void visit(Sif n) {
+	}
+
+	public void visit(Swhile n) {
+	}
+
+	public void visit(Sblock n) {
+	}
+
+	public void visit(Sreturn n) {
+	}
+
+	public void visit(Decl_fun n) {
+	}
+
+	public void visit(File n) {
+	}
+}
